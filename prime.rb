a53581bb87422207971af5192a3264157e9c2793
Anne Fausto <a.r.fausto@gmail.com>
2019-09-24 15:07:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
-def prime?(n)
+# prime?(n)
   if n < 2 
     false 
   else
     (2..n/2).none?{|i| n % 1 == 0}
     return true
   end
-end    
+#end    
+
+def prime(n)
+ (2..5).none? {|n| divisor % == 0} 
+end
